c0b5fe216f01244bef0040c6f1ce6237abba4240
Karol Phung <phungkarol@gmail.com>
2020-09-20 12:31:19 -0400

    Initial Commit

---
+++ FlixApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FlixApp
+//
+//  Created by Karol Phung on 9/20/20.
+//  Copyright © 2020 Karol Phung. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
